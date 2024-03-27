72e985896b92c94b0d6a9141ee7de51a05bad47a
CHINMAYEEHARANE58 <chinmayee.harane@kalvium.community>
2024-03-27 15:22:50 +0530

    enhancments made

--- Frontend/react/src/App.jsx
+++ Frontend/react/src/App.jsx
@@ -1,13 +1,17 @@
 import React from 'react'
 import './App.css'
 import Home from './Components/Home'
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
+
 
 function App() {
 
   return (
-    <>
-      <Home/>
-    </>
+      <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+      </Routes>
+      </BrowserRouter>
   )
 }
 
