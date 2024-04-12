4ee36317c89adc174173669897caeecebbd681b4
CHINMAYEEHARANE58 <chinmayee.harane@kalvium.community>
2024-04-12 15:21:29 +0530

    signin functionality compeleted and enhancments made

--- Backend/server.js
+++ Backend/server.js
@@ -7,7 +7,17 @@ const mongoServer = require('./config/db.js')
 
 
 const app = express();
-app.use(cors())
+app.use(
+    cors({
+      origin: function (origin, callback) {
+        if (!origin || allowedOrigins.includes(origin)) {
+          callback(null, true);
+        } else {
+          callback(new Error('Not allowed by CORS'));
+        }
+      },
+    })
+  );
 app.use(express.json());
 app.use("/", routes);
 
