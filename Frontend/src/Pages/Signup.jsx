abd8cf407fb45c1830e76bf80c644866fd104181
CHINMAYEEHARANE58 <chinmayee.harane@kalvium.community>
2024-04-12 14:48:24 +0530

    signin functionality compeleted

--- Frontend/src/Pages/Signup.jsx
+++ Frontend/src/Pages/Signup.jsx
@@ -1,44 +1,17 @@
 import {useState} from 'react'
 import './Signup.css'
 import { Link, useNavigate } from "react-router-dom";
-import axios from "axios";
 import Carousel from '../Components/Carousel'
 
 export default function Signup() {
 
-  const [signin, setSignin] = useState({
-    username: '',
-    email: '',
-    password: ''
-  });
+  const handleSubmit = (e) =>{
+    e.preventDefault();
+    navigate('/home');
+  }
 
   const navigate = useNavigate();
-
-  const handleChange = (e) => {
-    setSignin({
-      ...signin,
-      [e.target.name]: e.target.value
-    });
-  };
-
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-    try {
-      const response = await axios.post("https://s56-chinmayee-capstone-mitworking.onrender.com/register", signin);
-      if (response.status === 201) {
-        console.log("Registration successful");
-        alert("Registration successful");
-        navigate("/");
-      } else {
-        console.error("Registration failed");
-        alert("Registration failed");
-      }
-    } catch (error) {
-      console.error("Error:", error);
-      alert("Registration failed. Please try again.");
-    }
-  };
-
+  
   return (
     <>
     <div className='signup-page'>
@@ -47,15 +20,15 @@ export default function Signup() {
             <div className='signup-form'>
                 <h2 className='signup-title'>Signup</h2>
                 <div className='signup-input-box'>
-                    <input className='signup-username-input' type="text" name="username" value={signin.username} onChange={handleChange} required="required"/>
+                    <input className='signup-username-input' type="text" name="username" required="required"/>
                     <span>Username</span>
                 </div>
                 <div className='signup-input-box'>
-                    <input className='signup-password-input' type="text" name="email" value={signin.email} onChange={handleChange} required="required" />
+                    <input className='signup-password-input' type="email" name="email" required="required" />
                     <span>Email</span>
                 </div>
                 <div className='signup-input-box'>
-                    <input className='signup-password-input' type="password" name="password" value={signin.password} onChange={handleChange} required="required" />
+                    <input className='signup-password-input' type="password" name="password" required="required" />
                     <span>Password</span>
                 </div>
 
