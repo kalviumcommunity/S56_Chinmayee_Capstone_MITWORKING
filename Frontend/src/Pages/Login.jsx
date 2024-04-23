d425fce585b1ae880bb35244698e1bf6e1e6bc4b
CHINMAYEEHARANE58 <chinmayee.harane@kalvium.community>
2024-04-23 09:48:35 +0530

    login functionality in progress

--- Frontend/src/Pages/Login.jsx
+++ Frontend/src/Pages/Login.jsx
@@ -1,9 +1,40 @@
-import React from 'react'
+import {useState} from 'react'
 import './Login.css'
-import { Link } from "react-router-dom";
-import Carousel from '../Components/Carousel';
+import { Link, useNavigate } from "react-router-dom"
+import Carousel from '../Components/Carousel'
+import axios from 'axios'
 
 export default function Login() {
+  const [formData, setFormData] = useState({
+    username: '',
+    password: ''
+  })
+
+  const navigate = useNavigate()
+
+  const handleChange = (e) => {
+    setFormData({
+      ...formData,
+      [e.target.name]: e.target.value
+    })
+  }
+
+  const handleSubmit = async (e) => {
+    e.preventDefault()
+    try {
+      const response = await axios.post('/login', formData) 
+      if (response.status === 200) {
+        console.log('Login successful')
+        alert('Login successful')
+        navigate('/home')
+      } else {
+        console.error('Login failed')
+        alert('Login failed')
+      }
+    } catch (error) {
+      console.error('Error:', error)
+    }
+  }
 
   return (
     <>
@@ -13,15 +44,15 @@ export default function Login() {
             <div className='login-form'>
                 <h2 className='login-title'>Login</h2>
                 <div className='input-box'>
-                    <input className='username-input' type="text" required="required"/>
-                    <span>Username</span>
+                  <input className='username-input' type="text" name="username" value={formData.username} onChange={handleChange} required="required"/>
+                  <span>Username</span>
                 </div>
                 <div className='input-box'>
-                    <input className='password-input' type="password" required="required" />
-                    <span>Password</span>
+                  <input className='password-input' type="password" name="password" value={formData.password} onChange={handleChange} required="required" />
+                  <span>Password</span>
                 </div>
 
-                <Link to={"/home"}><button className='login-btn'>Login</button></Link>
+                <Link to={"/home"}><button className='login-btn' onClick={handleSubmit}>Login</button></Link>
                 <Link to={"/signup"}><h3 className='signup-text'>Don’t have an account? signup</h3></Link>
             </div>
         </div>
