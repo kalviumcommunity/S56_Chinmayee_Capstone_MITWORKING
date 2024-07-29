a038bc07d292a886fe94536d464e42c87ac82b61
CHINMAYEEHARANE58 <chinmayee.harane@kalvium.community>
2024-07-29 09:57:23 +0530

    Fixing the followers error

--- Frontend/src/Pages/Profile.jsx
+++ Frontend/src/Pages/Profile.jsx
@@ -32,15 +32,15 @@ export default function Profile() {
             <div className='profile-overveiw'>
             
                 <img className='prfPage-img' src={profile} alt={profile} />
-                <h2 className='prfPage-name'>{userData.name}</h2>
-                <h2 className='prfPage-username'>@{userData.username}</h2>
+                {/* <h2 className='prfPage-name'>{userData.name}</h2> */}
+                {/* <h2 className='prfPage-username'>@{userData.username}</h2> */}
 
                 <div className='prfPage-lines'>
-                    <h3 className='prfPage-numOfFollwers'>{userData.followers.length} <br />Followers</h3>
+                    {/* <h3 className='prfPage-numOfFollwers'>{userData.followers.length} <br />Followers</h3> */}
                     <div className='prfPage-middle-line1'></div>
-                    <h3 className='prfPage-numOfFollwing'>{userData.following.length} <br />Following</h3>
+                    {/* <h3 className='prfPage-numOfFollwing'>{userData.following.length} <br />Following</h3> */}
                     <div className='prfPage-middle-line2'></div>
-                    <h3 className='prfPage-numOfPost'>{userData.posts.length}<br/>Posts</h3>
+                    {/* <h3 className='prfPage-numOfPost'>{userData.posts.length}<br/>Posts</h3> */}
                 </div>
 
             </div>
