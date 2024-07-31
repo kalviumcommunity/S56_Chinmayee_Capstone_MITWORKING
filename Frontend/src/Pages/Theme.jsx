dd97d7ed13fce81b1bfdd67a30adcbafcc7ef647
CHINMAYEEHARANE58 <144110866+CHINMAYEEHARANE58@users.noreply.github.com>
2024-07-31 11:19:39 +0530

    Update Theme.jsx

--- Frontend/src/Pages/Theme.jsx
+++ Frontend/src/Pages/Theme.jsx
@@ -1,12 +1,1 @@
-import React, { createContext, useState, useEffect } from 'react'
-import Navbar from '../Components/Navbar'
-
-export default function Theme() {
-  return (
-    <div>
-      Theme page
-      <Navbar/>  
-    </div>
-  )
-}
 
