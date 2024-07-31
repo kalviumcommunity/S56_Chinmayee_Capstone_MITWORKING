b4e290a22b9a2e9c54282de048475fb082130376
CHINMAYEEHARANE58 <144110866+CHINMAYEEHARANE58@users.noreply.github.com>
2024-07-31 11:19:13 +0530

    Update Navbar.jsx

--- Frontend/src/Components/Navbar.jsx
+++ Frontend/src/Components/Navbar.jsx
@@ -48,11 +48,6 @@ export default function Navbar() {
             <h2 className='nav-text'>Messages</h2>
         </Link>
 
-        <Link to={'/theme'} className={`nav-content theme ${getActiveClass('/theme')}`}>
-            <img className='theme-icon icon' src={theme} alt="theme-icon" />
-            <h2 className='nav-text'>Theme</h2>
-        </Link>
-
         <button className='logout-btn' onClick={handleLogout}>Logout</button>      </nav>
     </>
   )
